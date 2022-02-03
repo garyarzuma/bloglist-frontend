f30504631ea22c41f117875c9411ffd4757ffdac
garyarzumanyan <garyarzumanyan@gmail.com>
2022-02-02 20:49:50 -0800

    Added logout and local storage. Finished 5.2

--- src/App.js
+++ src/App.js
@@ -16,6 +16,15 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
    
     event.preventDefault()
@@ -24,6 +33,9 @@ const App = () => {
       const user = await loginService.login({
         username, password
       })
+      window.localStorage.setItem(
+        'loggedBlogUser', JSON.stringify(user)
+      ) 
       blogService.setToken(user.token)
       setUser(user)
       setUsername('')
@@ -35,6 +47,13 @@ const App = () => {
       }, 5000) */
     }
   } 
+
+  const handleLogout = () => {
+    setUser(null)
+    setUsername('')
+    setPassword('')
+    window.localStorage.removeItem('loggedBlogUser')
+  }
 
   if (user === null) {
     return (
@@ -55,6 +74,7 @@ const App = () => {
     <div>
       <h2>blogs</h2>
       <p>{user.name} logged in</p>
+      <button onClick={()=>handleLogout()}>Logout</button>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
