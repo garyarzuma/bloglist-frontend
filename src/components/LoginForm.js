b8cb7ab9d726df1d8451cb5688c0df553dae6c95
garyarzumanyan <garyarzumanyan@gmail.com>
2022-02-07 23:56:21 -0800

    Added cypress and test for login form showing up. Finished 5.17

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -7,6 +7,7 @@ const LoginForm = ({ username, password, setPassword, setUsername, handlelogin }
       <div>
         username
         <input
+          id='username'
           type="text"
           value={username}
           name="Username"
@@ -16,13 +17,14 @@ const LoginForm = ({ username, password, setPassword, setUsername, handlelogin }
       <div>
         password
         <input
+          id='password'
           type="password"
           value={password}
           name="Password"
           onChange={({ target }) => setPassword(target.value)}
         />
       </div>
-      <button type="submit">login</button>
+      <button id='login-button' type="submit">login</button>
     </form>
   )
 }
